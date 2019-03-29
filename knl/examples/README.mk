0479035d84308321654d076dd3c5074040759b1a
Shaohao Chen <shaohao@bu.edu>
2019-03-29 13:26:50 -0400

    Add KNL

---
+++ knl/examples/README.mk
@@ -0,0 +1,16 @@
+<h3>RCS Examples for Intel Xeon Phi Knights Landing (KNL)</h3>
+
+<h4>Description of files</h4>
+
+<ul>
+<li><b>pi_openmp.c</b> - An OpenMP code to compute the value of pi. </li>
+<li><b>script.openmp</b> - A example job script to run OpenMP programs. </li>
+<li><b>pi_mpi.c</b> - An MPI code to compute the value of pi. </li>
+<li><b>script.mpi</b> - A example job script to run MPI programs. </li>
+</ul>
+
+<h4>Contact Information</h4>
+
+<em>help@scc.bu.edu</em><br>
+
+   
