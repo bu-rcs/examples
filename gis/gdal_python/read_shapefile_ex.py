35ab1fb76546caf1e9229653ce39c256cc34e122
Dennis Milechin <milechin@bu.edu>
2019-09-03 11:09:27 -0400

    Updated example to read_shapefile example.

--- gis/gdal_python/read_shapefile_ex.py
+++ gis/gdal_python/read_shapefile_ex.py
@@ -8,16 +8,17 @@ ogr.UseExceptions()
 driver = ogr.GetDriverByName("ESRI Shapefile")
 
 # Path to the shapefile
-shapefile='/projectnb/scv/milechin/git/examples/gis/data/shapefile/cb_2014_us_county_5m.shp'
+shapefile='/projectnb/scv/milechin/git/examples/gis/gdal_python/data/cb_2014_us_county_5m.shp'
 
 # Open the shapefile
 layer_conn = driver.Open(shapefile, 0)
 layer = layer_conn.GetLayer()
 
+# Set filter to extract the polygon representing the 
+# state of Massachusetts using FIPS code 25.
+layer.SetAttributeFilter("STATEFP='25'")
 
-# Set filter to extract the polygon representing the state of Massachusetts
-layer.SetAttributeFilter("STATEFP=25")
-
+# Print the counties in the state of Massachusetts
 print("Counties in State:")
 for feature in layer:
     print(feature.GetField("NAME"))
@@ -28,7 +29,6 @@ out_layer = driver.CreateDataSource("MA_counties.shp")
 
 # Copy filtered data to the new shapefile
 out_layer.CopyLayer(layer, 'massachusetts')
-
 
 
 
